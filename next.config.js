83b5bd683899187f7bceba6fe08cb49aa929d252
viswa3760 <viswa.bharath@dojima.network>
2024-11-22 18:41:02 +0530

    add omni loyalty path

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   basePath: "/omni-loyalty-program",
   assetPrefix: "/omni-loyalty-program",
   reactStrictMode: true,
-  images: { unoptimized: true, loader: "default", path: "" },
+  images: { unoptimized: true, loader: "default", path: "/omni-loyalty-program" },
   webpack: (config) => {
     config.resolve.modules.push(path.resolve("./"));
     config.module.rules.push({ test: /\.svg$/, use: ["@svgr/webpack"] });
