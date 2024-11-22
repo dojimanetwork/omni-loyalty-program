adbb16c20f2e57b774bf984617f3917854d1cc7a
viswa3760 <viswa.bharath@dojima.network>
2024-11-22 17:08:11 +0530

    update paths

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
+const path = require("path");
 
 module.exports = {
+  basePath: "/omni-loyalty-program",
+  assetPrefix: "/omni-loyalty-program",
   reactStrictMode: true,
   images: { unoptimized: true },
-  webpack: config => {
-    config.resolve.modules.push(path.resolve('./'))
-    config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] })
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve("./"));
+    config.module.rules.push({ test: /\.svg$/, use: ["@svgr/webpack"] });
 
-    return config
+    return config;
   },
-}
+};
